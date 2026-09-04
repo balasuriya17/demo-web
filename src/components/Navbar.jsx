d531f43d4a3370577af90d203c334d4513e627f7
suriya.jr <balasuriya1755@gmail.com>
2026-09-04 20:58:37 +0530

    Update Navbar links to include demo-web prefix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ function Navbar() {
       <div className="logo">DEMO GYM</div>
 
       <div className="nav-links">
-        <a href="/">HOME</a>
-        <a href="/#plan-details">PLANS</a>
-        <a href="/details">DETAILS</a>
-        <a href="/about">ABOUT</a>
-        <a href="/contact">CONTACT</a>
+        <a href="/demo-web/">HOME</a>
+        <a href="/demo-web/#plan-details">PLANS</a>
+        <a href="/demo-web/details">DETAILS</a>
+        <a href="/demo-web/about">ABOUT</a>
+        <a href="/demo-web/contact">CONTACT</a>
       </div>
     </nav>
   );
